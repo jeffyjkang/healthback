658ecbfd837012bf19b164a51985412f5af8bb42
jeffyjkang <jeffyjkang@gmail.com>
2019-07-10 23:10:24 -0700

    update put route for goals

--- api/routes/goalRoute.js
+++ api/routes/goalRoute.js
@@ -23,6 +23,7 @@ router.post("/", auth.authorize, async (req, res) => {
   const id = decodedToken.id;
   const goal = req.body;
   goal.userId = id;
+  console.log(goal);
   if (!decodedToken) {
     return res
       .status(401)
@@ -36,6 +37,43 @@ router.post("/", auth.authorize, async (req, res) => {
     res.status(201).json(createdGoal);
   } catch (error) {
     res.status(500).json({ error: "There was an error creating a goal." });
+  }
+});
+// edit goal
+router.put("/:id", auth.authorize, async (req, res) => {
+  const id = req.params.id;
+  const decodedToken = req.decodedToken;
+  const userId = decodedToken.id;
+  const goal = req.body;
+  goal.userId = userId;
+  if (!id) {
+    return res.status(400).json({ error: "Must provide goal id." });
+  }
+  if (!decodedToken) {
+    return res
+      .status(401)
+      .json({ error: "There was an error decoding the token" });
+  }
+  if (!userId) {
+    res.status(400).json({ error: "Must provide user id." });
+  }
+  try {
+    const requestedGoal = await goalModel.get(id).first();
+    if (requestedGoal.userId !== userId) {
+      return res
+        .status(401)
+        .json({ erro: "Must be creator of goal to modify goal." });
+    }
+    const updatedGoal = await goalModel.edit(id, { ...req.body });
+    if (!updatedGoal) {
+      return res
+        .status(400)
+        .json({ error: "The goal with the specified id does not exist." });
+    } else {
+      res.status(200).json(updatedGoal);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "There was an error updaing the goal." });
   }
 });
 
