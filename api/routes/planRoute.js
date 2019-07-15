858b077607fb55966b5c2f491cfa79bbda726f28
jeffyjkang <jeffyjkang@gmail.com>
2019-07-14 22:49:02 -0700

    update plan delete route

--- api/routes/planRoute.js
+++ api/routes/planRoute.js
@@ -65,7 +65,7 @@ router.put("/:id", auth.authorize, async (req, res) => {
     return res.status(400).json({ error: "Must provide user id." });
   }
   if (!plan.goalId) {
-    return res.status(400).json({ error: "Must provide goal id" });
+    return res.status(400).json({ error: "Must provide goal id." });
   }
   try {
     const requestedGoal = await goalModel.get(plan.goalId).first();
@@ -84,6 +84,42 @@ router.put("/:id", auth.authorize, async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({ error: "There was an error updating the plan." });
+  }
+});
+// delete plan
+router.delete("/:id", auth.authorize, async (req, res) => {
+  const id = req.params.id;
+  const decodedToken = req.decodedToken;
+  const userId = decodedToken.id;
+  if (!id) {
+    return res.status(400).json({ error: "Must provide plan id." });
+  }
+  if (!decodedToken) {
+    return res
+      .status(401)
+      .json({ error: "There was an error decoding the token." });
+  }
+  if (!userId) {
+    return res.status(400).json({ error: "Must provide user id." });
+  }
+  try {
+    const requestedPlan = await planModel.get(id).first();
+    const requestedGoal = await goalModel.get(requestedPlan.goalId).first();
+    if (requestedGoal.userId !== userId) {
+      return res
+        .status(401)
+        .json({ error: "Must be creator of goal and plan to delete plan." });
+    }
+    const deletedPlan = await planModel.remove(id);
+    if (!deletedPlan) {
+      return res
+        .status(400)
+        .json({ error: "The plan with the specified id does not exist." });
+    } else {
+      res.status(200).json({ message: "Plan deleted." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "There was an error deleting the plan." });
   }
 });
 //
