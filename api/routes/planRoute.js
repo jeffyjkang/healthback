4a9927e74cf98b3336907d90646af3a3a2285fc3
jeffyjkang <jeffyjkang@gmail.com>
2019-07-14 22:03:52 -0700

    update plan edit route

--- api/routes/planRoute.js
+++ api/routes/planRoute.js
@@ -51,8 +51,40 @@ router.post("/", auth.authorize, async (req, res) => {
 router.put("/:id", auth.authorize, async (req, res) => {
   const id = req.params.id;
   const decodedToken = req.decodedToken;
-  userId = decodedToken.id;
+  const userId = decodedToken.id;
   const plan = req.body;
+  if (!id) {
+    return res.status(400).json({ error: "Must provide plan id" });
+  }
+  if (!decodedToken) {
+    return res
+      .status(401)
+      .json({ error: "There was an error decoding the token." });
+  }
+  if (!userId) {
+    return res.status(400).json({ error: "Must provide user id." });
+  }
+  if (!plan.goalId) {
+    return res.status(400).json({ error: "Must provide goal id" });
+  }
+  try {
+    const requestedGoal = await goalModel.get(plan.goalId).first();
+    if (requestedGoal.userId !== userId) {
+      return res
+        .status(401)
+        .json({ error: "Must be creator of goal and plan to modify plan." });
+    }
+    const updatedPlan = await planModel.edit(id, { ...req.body });
+    if (!updatedPlan) {
+      return res
+        .status(400)
+        .json({ error: "The plan with the specified id does not exist." });
+    } else {
+      res.status(200).json(updatedPlan);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "There was an error updating the plan." });
+  }
 });
 //
 module.exports = router;
